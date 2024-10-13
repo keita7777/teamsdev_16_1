2cd4fee6949a0fbe585f35e8d52b9105c16c7e20
imaimai <onakasuitanaapple@gmail.com>
2024-10-13 20:05:27 +0900

    app/page.tsxの変更分を初期に戻す

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Pagination from "@/components/Pagination";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <p>Hello World!</p>
-      <Pagination />
-    </>
-  );
+  return <p>Hello World!</p>;
 }
