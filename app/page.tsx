60f4831a45a616ed17f34b01672dd8e57e9f0c1e
as-0908 <tamagokake5han014@gmail.com>
2024-10-13 21:27:45 +0900

    Headerコンポーネント読み込みを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Header from "../components/Header";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Header />
       <p>Hello World!</p>
-    </>
   );
 }
