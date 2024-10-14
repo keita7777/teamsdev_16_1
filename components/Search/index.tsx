4fdbd35519f9cd3758ac95d44162d82c51946ebb
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-14 14:40:41 +0900

    コード修正

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -2,23 +2,19 @@ import React from "react";
 import styles from "./style.module.css";
 import { FaSearch } from "react-icons/fa";
 
-function Page() {
+function Search() {
   return (
-    <>
-      <div className={styles.container}>
         <div className={styles.search}>
           <input
             className={styles.input}
             type="text"
             placeholder="検索"
-          ></input>
+          />
           <div>
             <FaSearch className={styles.SearchIcon} />
           </div>
         </div>
-      </div>
-    </>
   );
 }
 
-export default Page;
+export default Search;
