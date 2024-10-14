58bf151756bd9711ecc67037fe571088c325fecd
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-15 06:52:21 +0900

    検索フォーム作成 (#15)
    * 検索バーを作成
    
    * ディレクトリ変更
    
    * 検索バー作成
    
    * パッケージ削除
    
    * パッケージ削除
    
    * パッケージをインストール
    
    * パッケージ削除
    
    * パッケージインストールしました
    
    * React-iconsに変更
    
    * コード修正
    
    * コード修正

---
+++ components/Search/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./style.module.css";
+import { FaSearch } from "react-icons/fa";
+
+function Search() {
+  return (
+    <div className={styles.search}>
+      <input className={styles.input} type="text" placeholder="検索" />
+      <div>
+        <FaSearch className={styles.SearchIcon} />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
