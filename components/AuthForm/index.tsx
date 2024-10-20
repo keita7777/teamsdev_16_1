f1cf9e71b2244ae5dade0fbc83f7d984a0bc678b
imaimai <onakasuitanaapple@gmail.com>
2024-10-20 17:31:22 +0900

    サインアップとサインインの切替を追加

---
+++ components/AuthForm/index.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./styles.module.css";
+
+type props = {
+  isSignUp: boolean;
+};
+
+const AuthForm: React.FC<props> = ({ isSignUp }) => {
+  return (
+    <div className={styles.wrapper}>
+      <h2>{isSignUp ? "Sign Up" : "Sign In"}</h2>
+      <form className={styles.formContainer}>
+        {/* サインアップ時のみ名前入力を表示 */}
+        {isSignUp && (
+          <>
+            <p className={styles.inputTitle}>Name</p>
+            <input type="text" name="name" placeholder="Enter your name" />
+          </>
+        )}
+        <p className={styles.inputTitle}>Email</p>
+        <input type="email" name="email" placeholder="Enter your email" />
+        <p className={styles.inputTitle}>Password</p>
+        <input type="password" name="password" placeholder="Enter your password" />
+      </form>
+      <button type="submit" className={styles.submitButton}>
+        {isSignUp ? "Sign Up" : "Sign In"}
+      </button>
+
+      {isSignUp ? (
+        <p className={styles.comment}>
+          {/* サインアップ時のコメント */}
+          Already have an account?
+          <Link href="/signin">
+            <span>Sign In</span>
+          </Link>
+        </p>
+      ) : (
+        <p className={styles.comment}>
+          {/* サインイン時のコメント */}
+          Don&apos;t have an account?
+          <Link href="/signup">
+            <span>Sign Up</span>
+          </Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default AuthForm;
