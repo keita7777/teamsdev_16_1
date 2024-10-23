94f9ab53191d338124369bd7b395071ad958e41f
Oma2 <160988318+as-0908@users.noreply.github.com>
2024-10-23 19:39:20 +0900

    Fe/feature/moreposts (#30)
    * ヘッダー作成
    
    * ヘッダーの高さ指定を削除
    
    * prettier の設定を変更後、npm run format実行
    
    * Headerコンポーネント読み込みを削除
    
    * headerにpadding追加
    
    * アイコンをreact-icons使用に変更
    
    * more postsを作成
    
    * テストデータパス変更

---
+++ components/MorePosts/index.tsx
@@ -0,0 +1,35 @@
+import styles from "./styles.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { TestData } from "@/DummyData/ArticleData";
+import ArticleImage from "@/DummyData/article-image.png";
+
+type Post = {
+  id: string;
+  title: string;
+  imageUrl: string;
+};
+
+type MorePostsProps = {
+  posts: Post[];
+};
+
+const MorePosts = ({ posts }: MorePostsProps) => {
+  return (
+    <div className={styles.wrap}>
+      <h3 className={styles.title}>More Posts</h3>
+      <div className={styles.morePostsContainer}>
+        {TestData.slice(2, 5).map((post) => (
+          <div key={post.id} className={styles.postCard}>
+            <Link href={`/posts/${post.id}`}>
+              <Image src={ArticleImage} alt={post.title} className={styles.postImage} width={300} height={200} />
+              <h4 className={styles.postTitle}>{post.title}</h4>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MorePosts;
