119345bb8f276ccb21c8d6242dbb7e7e76ef6a24
imaimai <onakasuitanaapple@gmail.com>
2024-10-27 19:24:26 +0900

    データの渡し方、型定義を修正

--- components/MorePosts/index.tsx
+++ components/MorePosts/index.tsx
@@ -1,13 +1,16 @@
 import styles from "./styles.module.css";
 import Link from "next/link";
-import Image from "next/image";
-import { TestData } from "@/DummyData/ArticleData";
+import Image, { StaticImageData } from "next/image";
 import ArticleImage from "@/DummyData/article-image.png";
 
 type Post = {
-  id: string;
+  id: number;
   title: string;
-  imageUrl: string;
+  content: string;
+  category: string;
+  image_path: string | StaticImageData;
+  author: string;
+  created_at: string;
 };
 
 type MorePostsProps = {
@@ -19,7 +22,7 @@ const MorePosts = ({ posts }: MorePostsProps) => {
     <div className={styles.wrap}>
       <h3 className={styles.title}>More Posts</h3>
       <div className={styles.morePostsContainer}>
-        {TestData.slice(2, 5).map((post) => (
+        {posts.slice(0, 3).map((post) => (
           <div key={post.id} className={styles.postCard}>
             <Link href={`/posts/${post.id}`}>
               <Image src={ArticleImage} alt={post.title} className={styles.postImage} width={300} height={200} />
