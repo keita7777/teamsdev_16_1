880a822c489feded25a9bfbe26bc898078831e26
Oma2 <160988318+as-0908@users.noreply.github.com>
2024-10-30 19:52:35 +0900

    ホーム画面 (#45)
    * ヘッダー作成
    
    * ヘッダーの高さ指定を削除
    
    * prettier の設定を変更後、npm run format実行
    
    * Headerコンポーネント読み込みを削除
    
    * headerにpadding追加
    
    * アイコンをreact-icons使用に変更
    
    * more postsを作成
    
    * テストデータパス変更
    
    * TOPページ 表示、各コンポーネントのCSS変更
    
    * 検索フォームとページネーションのサイズ変更
    
    * <></> Fragmentに変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Header from "@/components/Header";
+import Search from "@/components/Search";
+import ArticleList from "@/components/ArticleList";
+import Pagination from "@/components/Pagination";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return (
+    <>
+      <Header />
+      <Search />
+      <ArticleList />
+      <Pagination />
+    </>
+  );
 }
