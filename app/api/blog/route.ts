e1d13e071bdef5a5556a5bf8ca715109133fc8c1
github keita7777 <github mine0kei@gmail.com>
2024-11-01 21:09:40 +0900

    ブログ投稿用のAPIエンドポイントを作成

---
+++ app/api/blog/route.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const supabase = await createClient();
+  const data = await req.json();
+  await supabase.from("posts").insert(data);
+
+  try {
+    return NextResponse.json({ message: "投稿完了" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "投稿失敗", error }, { status: 500 });
+  }
+};
