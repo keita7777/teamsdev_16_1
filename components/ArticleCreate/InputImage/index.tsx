c926660fce06f396cbbe2deed32aacca2667f1ba
imaimai <onakasuitanaapple@gmail.com>
2024-11-03 10:00:42 +0900

    画像プレビュー機能の追加

---
+++ components/ArticleCreate/InputImage/index.tsx
@@ -0,0 +1,12 @@
+import React, { forwardRef, InputHTMLAttributes } from "react";
+
+type Props = {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  id: InputHTMLAttributes<HTMLInputElement>["id"];
+};
+
+const InputImage = forwardRef<HTMLInputElement, Props>(function InputImage({ id, onChange }, ref) {
+  return <input ref={ref} id={id} type="file" accept="image/*" onChange={onChange} hidden />;
+});
+
+export default InputImage;
