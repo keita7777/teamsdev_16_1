58409e9d2240b6b1f99759d94deb076da318c542
github keita7777 <github mine0kei@gmail.com>
2024-11-04 16:34:50 +0900

    try文の記述位置修正

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,11 +2,10 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  const supabase = await createClient();
-  const data = await req.json();
-  await supabase.from("posts").insert(data);
-
   try {
+    const supabase = await createClient();
+    const data = await req.json();
+    await supabase.from("posts").insert(data);
     return NextResponse.json({ message: "投稿完了" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "投稿失敗", error }, { status: 500 });
