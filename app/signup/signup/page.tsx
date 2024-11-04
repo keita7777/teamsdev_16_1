77a48e6c907a340992f6818284614c1ebda11ebb
MATSUMURA NAMI <nami-t-73@hotmail.co.jp>
2024-11-05 00:05:14 +0900

    サインアップ画面　作成

--- app/signup/signup/page.tsx
+++ app/signup/signup/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../components/Header';
-import SignUpForm from '../components/AuthForm/SignUpForm';
+import Header from '../../../components/Header';
+import SignUpForm from '../../../components/AuthForm/SignUpForm';
 
 const SignUpPage: React.FC = () => {
   return (
