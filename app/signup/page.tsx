0bb9635f4feb0befe2310040d7915d179c8d65e7
nami73kan <nami-t-73@hotmail.co.jp>
2024-11-06 20:43:49 +0900

    Fe/feature/sign up (#62)
    * サインアップ画面　作成
    
    * Add SignUp form
    
    * サインアップ画面　作成
    
    * サインアップ画面　修正

---
+++ app/signup/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../../components/Header';
+import SignUpForm from '../../components/AuthForm/SignUpForm';
+import styles from './styles.module.css';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
+
+
