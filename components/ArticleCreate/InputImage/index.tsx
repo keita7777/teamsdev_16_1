59743a0b461dbcf175fedf14458333a355730af7
keita7777 <47020745+keita7777@users.noreply.github.com>
2024-11-09 00:00:21 +0900

    [BE]ブログ記事作成機能 (#57)
    * supabase、react-hooks-formの導入
    
    * supabaseから型定義を取得
    
    * supabaseの初期化処理
    
    * ブログ投稿用のAPIエンドポイントを作成
    
    * ブログ新規作成ページ
    
    * supabaseのstorageに画像をアップロードする処理
    
    * ブログ新規作成処理
    
    * スタイル調整
    
    * disabledの条件修正
    
    * disabledの条件修正
    
    * 画像プレビュー機能をmerge、コンフリクト解消
    
    * onSubmit内のcreateBlogにawaitを追加
    
    * コメントアウト、console.log削除
    
    * ブログ新規作成フォームの型定義を作成
    
    * InputImageコンポーネントのregisterの型を定義
    
    * try文の記述位置修正
    
    * .env.example作成
    
    ---------
    
    Co-authored-by: github keita7777 <github mine0kei@gmail.com>
    Co-authored-by: shtk0llq <shtk0llq@gmail.com>

--- components/ArticleCreate/InputImage/index.tsx
+++ components/ArticleCreate/InputImage/index.tsx
@@ -1,12 +1,38 @@
+import { FormValues } from "@/type/articleCraeteFormType";
 import React, { forwardRef, InputHTMLAttributes } from "react";
+import { UseFormRegister } from "react-hook-form";
 
 type Props = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   id: InputHTMLAttributes<HTMLInputElement>["id"];
+  isSubmitting: boolean;
+  register: UseFormRegister<FormValues>;
 };
 
-const InputImage = forwardRef<HTMLInputElement, Props>(({ id, onChange }, ref) => {
-  return <input ref={ref} id={id} type="file" accept="image/*" onChange={onChange} hidden />;
+const InputImage = forwardRef<HTMLInputElement, Props>(({ id, onChange, isSubmitting, register }) => {
+  const {
+    onChange: registerOnChange,
+    ref: registerRef,
+    ...registerExceptOnchangeAndRef
+  } = register("file", {
+    required: "画像を選択してください",
+  });
+
+  return (
+    <input
+      ref={registerRef}
+      id={id}
+      type="file"
+      accept="image/*"
+      onChange={(event) => {
+        registerOnChange(event);
+        onChange(event);
+      }}
+      hidden
+      disabled={isSubmitting}
+      {...registerExceptOnchangeAndRef}
+    />
+  );
 });
 
 export default InputImage;
