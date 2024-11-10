1a020aff570ea713b598f5c3bfcf261dec79c39d
imaimai <onakasuitanaapple@gmail.com>
2024-11-10 20:16:04 +0900

    詳細ページのデータ取得

---
+++ app/api/[id]/route.ts
@@ -0,0 +1,24 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from 'next/server';
+
+
+export const GET = async(request:Request,{params}:{ params: Promise<{ id: string }> }) => {
+
+
+  try{
+    const id = (await params).id;    
+    const supabaseData = await createClient();
+  const {data, error } = await supabaseData.from("posts").select("*"
+  ).eq("id",id).single();
+  
+
+  if (error) {
+    throw error;
+  }
+
+  return NextResponse.json({ message: "取得完了", posts: data }, { status: 200 });
+} catch (error) {
+  return NextResponse.json({ message: "取得失敗", error }, { status: 500 });
+}
+};
+  
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,17 +1,45 @@
+"use client";
 import ArticleDetails from "@/components/ArticleDetails";
 import Comments from "@/components/Comments";
 import Header from "@/components/Header";
 import MorePosts from "@/components/MorePosts";
 import { TestData } from "@/DummyData/ArticleData";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.css";
+import { Post } from "@/type/post";
 
-export default function Posts() {
+export default function Posts({ params }: { params: { id: string } }) {
+  const [post, setPost] = useState<Post | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const id = params.id;
+
+        const response = await fetch(`/api/${id}`);
+        if (!response.ok) {
+          router.replace("/404");
+        }
+        const result = await response.json();
+        console.log(result);
+        if (!result.posts) {
+          router.replace("/404");
+        }
+        setPost(result.posts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPost();
+  }, []);
+
   return (
     <>
       <Header />
       <div className={styles.container}>
-        <ArticleDetails />
+        {post ? <ArticleDetails articleData={post} /> : <p>Loading...</p>}
         <MorePosts posts={TestData} />
         <Comments />
       </div>
