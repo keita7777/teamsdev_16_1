25622479542fab852283ca5d14fb610289db206c
github keita7777 <github mine0kei@gmail.com>
2024-11-12 04:24:13 +0900

    firebaseにユーザーを作成する処理

--- components/AuthForm/index.tsx
+++ components/AuthForm/index.tsx
@@ -1,31 +1,87 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import styles from "./styles.module.css";
+import { useForm } from "react-hook-form";
+import { signupFormType } from "@/type/authFormType";
+import { signUpWithEmail } from "@/utils/firebase/auth";
 
 type Props = {
   isSignUp: boolean;
 };
 
 const AuthForm = ({ isSignUp }: Props) => {
+  const {
+    handleSubmit,
+    register,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<signupFormType>();
+
+  const onSubmit = async (data: signupFormType) => {
+    const { name, email, password } = data;
+    try {
+      const response = await signUpWithEmail(email, password);
+      console.log(response.user.uid);
+    } catch (error: any) {
+      if (
+        error.message === "このメールアドレスは既に使用されています" ||
+        error.message === "無効なメールアドレスです"
+      ) {
+        setError("email", { message: error.message });
+      } else if (error.message === "パスワードは6文字以上にしてください") {
+        setError("password", { message: error.message });
+      } else {
+        setError("root", { message: error.message });
+      }
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <h2>{isSignUp ? "Sign Up" : "Sign In"}</h2>
-      <form className={styles.formContainer}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
+        {errors.root && <p className={styles.errorMessage}>{errors.root.message?.toString()}</p>}
         {/* サインアップ時のみ名前入力を表示 */}
         {isSignUp && (
           <>
             <p className={styles.inputTitle}>Name</p>
-            <input type="text" name="name" placeholder="Enter your name" />
+            <input
+              type="text"
+              placeholder="Enter your name"
+              {...register("name", { required: "名前を入力してください" })}
+            />
+            {errors.name && <p className={styles.errorMessage}>{errors.name.message?.toString()}</p>}
           </>
         )}
         <p className={styles.inputTitle}>Email</p>
-        <input type="email" name="email" placeholder="Enter your email" />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          {...register("email", {
+            required: "メールアドレスを入力してください",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "無効なメールアドレスです",
+            },
+          })}
+        />
+        {errors.email && <p className={styles.errorMessage}>{errors.email.message?.toString()}</p>}
         <p className={styles.inputTitle}>Password</p>
-        <input type="password" name="password" placeholder="Enter your password" />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          {...register("password", {
+            required: "パスワードを入力してください",
+            minLength: { value: 6, message: "パスワードは6文字以上にしてください" },
+          })}
+        />
+        {errors.password && <p className={styles.errorMessage}>{errors.password.message?.toString()}</p>}
+        <button type="submit" className={styles.submitButton}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </button>
       </form>
-      <button type="submit" className={styles.submitButton}>
-        {isSignUp ? "Sign Up" : "Sign In"}
-      </button>
 
       {isSignUp ? (
         <p className={styles.comment}>
