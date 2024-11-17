5c7f2812a1772db36ebea9709bd9856f5f3f1116
yoshida0522 <kantama2006@yahoo.co.jp>
2024-11-17 09:16:39 +0900

    [BE]コメント機能 (#73)
    * 記事作成フォームの作成
    
    * エンドポイントとAPIの記述追加
    
    * コード修正
    
    * コンフリクト解消
    
    * 不要箇所削除
    
    * コメント機能作成

--- components/Comments/CommentForm/index.tsx
+++ components/Comments/CommentForm/index.tsx
@@ -1,9 +1,58 @@
+"use client";
+import { useState } from "react";
 import styles from "./styles.module.css";
+import { useRouter, useParams } from "next/navigation";
 
 const CommentForm = () => {
+  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
+  const params = useParams();
+  const postId = params.id;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const insertData = {
+      comment: inputValue,
+      // ユーザー情報が取得できるようになり次第、動的になるよう変更
+      user_id: "1",
+      post_id: postId,
+      created_at: new Date().toISOString(),
+    };
+
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(insertData),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      setInputValue("");
+      alert(result.message);
+      router.refresh();
+    } else {
+      alert("Error: " + result.error);
+    }
+  };
+
   return (
-    <form className={styles.commentForm}>
-      <input className={styles.commentInput} type="text" name="comment" placeholder="Your comment..." />
+    <form className={styles.commentForm} onSubmit={handleSubmit}>
+      <input
+        className={styles.commentInput}
+        type="text"
+        name="comment"
+        value={inputValue}
+        placeholder="Your comment..."
+        onChange={handleInputChange}
+      />
       <button className={styles.commentBtn}>Comment</button>
     </form>
   );
--- components/Comments/index.tsx
+++ components/Comments/index.tsx
@@ -2,18 +2,56 @@ import CommentCard from "./CommentCard";
 import CommentForm from "./CommentForm";
 import styles from "./styles.module.css";
 
-// テストデータ
-import { CommentData } from "@/DummyData/CommentData";
+type UserComment = {
+  id: string;
+  post_id: string;
+  comment: string;
+  created_at: string;
+  users: {
+    name: string;
+    email: string;
+    profileImg: string;
+  };
+};
 
-const Comments = () => {
+const fetchComments = async (): Promise<UserComment[]> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comments`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("データの取得に失敗しました");
+  }
+
+  const { comments }: { comments: UserComment[] } = await res.json();
+  return comments;
+};
+
+const Comments = async () => {
+  let comments: UserComment[] = [];
+  let errorMessage: string | null = null;
+
+  try {
+    comments = await fetchComments();
+  } catch (error) {
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else {
+      errorMessage = "エラーが発生しました";
+    }
+  }
+
   return (
     <div className={styles.commentsContainer}>
       <h2>Comments</h2>
       <CommentForm />
-      {CommentData.map((comment) => (
-        <CommentCard key={comment.id} comment={comment} />
-      ))}
+      {errorMessage ? (
+        <div className={styles.errorMessage}>{errorMessage}</div>
+      ) : (
+        comments.map((data) => <CommentCard key={data.id} comment={data} users={data.users} />)
+      )}
     </div>
   );
 };
+
 export default Comments;
