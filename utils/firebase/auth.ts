f9f0eb13a821060e4405c6b6ba785a5b54fbf941
github keita7777 <github mine0kei@gmail.com>
2024-11-20 01:24:46 +0900

    サインイン処理

--- utils/firebase/auth.ts
+++ utils/firebase/auth.ts
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./config";
 
 export const signUpWithEmail = async (email: string, password: string) => {
@@ -26,5 +26,25 @@ export const signUpWithEmail = async (email: string, password: string) => {
     }
 
     throw new Error(errorMessage);
+  }
+};
+
+export const signInWithEmail = async (email: string, password: string) => {
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    return user;
+  } catch (error: any) {
+    let errorMessage = "サインインに失敗しました";
+    if (error.code) {
+      switch (error.code) {
+        case "auth/invalid-credential":
+          errorMessage = "メールアドレスかパスワードが間違っています";
+          break;
+        default:
+          errorMessage = "予期しないエラーが発生しました";
+          break;
+      }
+      throw new Error(errorMessage);
+    }
   }
 };
