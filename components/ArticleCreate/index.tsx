ea89c57ddaff978199557b62442410a39941f0a1
imaimai <onakasuitanaapple@gmail.com>
2024-11-23 16:00:55 +0900

    editの更新

--- components/ArticleCreate/index.tsx
+++ components/ArticleCreate/index.tsx
@@ -10,19 +10,30 @@ import { useRouter } from "next/navigation";
 import InputImage from "./InputImage";
 import { useGetImageUrl } from "./hooks/useGetImageUrl";
 import { FormValues } from "@/type/articleCraeteFormType";
+import { Post } from "@/type/post";
+
+type EditPostProps = {
+  editPost?: Post;
+};
 
 const IMAGE_ID = "imageId";
 
-const ArticleCreate = () => {
+const ArticleCreate = ({ editPost }: EditPostProps) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [imageFile, setImageFile] = useState<File | null>(null);
+  const [editImagePath, setEditImagePath] = useState<string | null>(editPost?.image_path || null);
   const {
     handleSubmit,
     register,
     setError,
     resetField,
     formState: { errors, isSubmitting },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    defaultValues: {
+      title: editPost?.title || "",
+      content: editPost?.content || "",
+    },
+  });
   const router = useRouter();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,6 +47,7 @@ const ArticleCreate = () => {
   const { imageUrl } = useGetImageUrl({ file: imageFile });
   const handleClickCancelButton = () => {
     setImageFile(null);
+    setEditImagePath(null);
     // useFormで管理しているfileをリセット
     resetField("file");
     if (fileInputRef.current) {
@@ -69,21 +81,48 @@ const ArticleCreate = () => {
     }
   };
 
+  const uploadBlog = async (title: string, content: string, imagePath: string) => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/${editPost?.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+          image_path: imagePath,
+        }),
+      });
+      if (!response.ok) {
+        return { message: "エラーが発生しました" };
+      }
+    } catch (error) {
+      return { message: "エラーが発生しました", error };
+    }
+  };
+
   const onSubmit = async (data: FieldValues) => {
     const { title, content, file } = data;
     const imagePath = file?.[0];
 
-    if (!imagePath.type.startsWith("image/")) {
+    if ((!imagePath && !editImagePath) || (imagePath && !imagePath.type.startsWith("image/"))) {
       return setError("file", {
         message: "画像ファイルを選択してください",
       });
     }
 
     try {
-      const fullPath = await uploadImage(imagePath);
-      await createBlog(title, content, fullPath);
-      router.push("/");
-      router.refresh();
+      const fullPath =
+        imagePath && imagePath.type.startsWith("image/") ? await uploadImage(imagePath) : editImagePath || "";
+      if (editPost) {
+        await uploadBlog(title, content, fullPath);
+        router.back();
+      } else {
+        await createBlog(title, content, fullPath);
+        router.push("/");
+        router.refresh();
+      }
       return { message: "投稿完了" };
     } catch (error) {
       return { message: "投稿失敗", error };
@@ -96,6 +135,7 @@ const ArticleCreate = () => {
         <input
           className={styles.titleInput}
           placeholder="Title"
+          id="title"
           disabled={isSubmitting}
           {...register("title", { required: "タイトルを入力してください" })}
         />
@@ -104,6 +144,8 @@ const ArticleCreate = () => {
       <label htmlFor={IMAGE_ID} className={styles.fileWrapper}>
         {imageUrl && imageFile ? (
           <img src={imageUrl} alt="アップロード画像" className={styles.uploadImage} />
+        ) : editImagePath ? (
+          <img src={editImagePath} alt="編集画像" className={styles.uploadImage} />
         ) : (
           <div className={styles.uploadContainer}>
             <span className={styles.uploadIcon}>
@@ -118,13 +160,14 @@ const ArticleCreate = () => {
           onChange={handleFileChange}
           isSubmitting={isSubmitting}
           register={register}
+          editImagePath={editImagePath}
         />
       </label>
-      {imageUrl && imageFile && (
+      {(imageUrl && imageFile) || editImagePath ? (
         <button onClick={handleClickCancelButton} className={styles.cancelText}>
           画像キャンセル
         </button>
-      )}
+      ) : null}
 
       {errors.file && <p className={styles.errorMessage}>{errors.file.message?.toString()}</p>}
       <div className={styles.contentWrapper}>
@@ -132,13 +175,14 @@ const ArticleCreate = () => {
           {...register("content", { required: "記事内容を入力してください" })}
           className={styles.contentInput}
           placeholder="Text"
+          id="content"
           disabled={isSubmitting}
         ></textarea>
       </div>
       {errors.content && <p className={styles.errorMessage}>{errors.content.message?.toString()}</p>}
       <div className={styles.buttonWrapper}>
         <button type="submit" className={styles.createButton} disabled={isSubmitting}>
-          Create
+          {editPost ? "Upload" : "Create"}
         </button>
       </div>
     </form>
