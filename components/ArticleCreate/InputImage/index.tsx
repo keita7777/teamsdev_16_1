42ac41c26f2060a3f08fff4736b237ad99a01c1f
imaimai <onakasuitanaapple@gmail.com>
2024-11-23 16:01:32 +0900

    エラー解消

--- components/ArticleCreate/InputImage/index.tsx
+++ components/ArticleCreate/InputImage/index.tsx
@@ -7,33 +7,45 @@ type Props = {
   id: InputHTMLAttributes<HTMLInputElement>["id"];
   isSubmitting: boolean;
   register: UseFormRegister<FormValues>;
+  editImagePath?: string | null;
 };
 
-const InputImage = forwardRef<HTMLInputElement, Props>(({ id, onChange, isSubmitting, register }) => {
-  const {
-    onChange: registerOnChange,
-    ref: registerRef,
-    ...registerExceptOnchangeAndRef
-  } = register("file", {
-    required: "画像を選択してください",
-  });
+const InputImage = forwardRef<HTMLInputElement, Props>(
+  ({ id, onChange, isSubmitting, register, editImagePath }, ref) => {
+    const {
+      onChange: registerOnChange,
+      ref: registerRef,
+      ...registerExceptOnchangeAndRef
+    } = register("file", {
+      required: !editImagePath && "画像を選択してください",
+    });
 
-  return (
-    <input
-      ref={registerRef}
-      id={id}
-      type="file"
-      accept="image/*"
-      onChange={(event) => {
-        registerOnChange(event);
-        onChange(event);
-      }}
-      hidden
-      disabled={isSubmitting}
-      {...registerExceptOnchangeAndRef}
-    />
-  );
-});
+    const inputRef = (element: HTMLInputElement | null) => {
+      registerRef(element); // React Hook Formのregisterのref
+      if (typeof ref === "function") {
+        ref(element); // forwardRefのref
+      } else if (ref) {
+        (ref as React.MutableRefObject<HTMLInputElement | null>).current = element;
+      }
+    };
+
+    return (
+      <input
+        ref={inputRef}
+        id={id}
+        type="file"
+        accept="image/*"
+        onChange={(event) => {
+          registerOnChange(event);
+          onChange(event);
+        }}
+        hidden
+        disabled={isSubmitting}
+        {...registerExceptOnchangeAndRef}
+      />
+    );
+  },
+);
 
 InputImage.displayName = "InputImage";
 export default InputImage;
--- type/articleCraeteFormType.ts
+++ type/articleCraeteFormType.ts
@@ -1,5 +1,5 @@
 export type FormValues = {
   title: string;
   content: string;
-  file: FileList;
+  file: FileList|null;
 };
